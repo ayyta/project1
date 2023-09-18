ffb10336b01d37637dc45f13b511c65ab7a79fa2
Alvis Yong <yong.mark.alvis@gmail.com>
2023-09-18 16:57:58 -0700

    Create Components Folder and linked files. TODO: Fix grid and add information to each card

---
+++ src/components/Event.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Event = (props) => {
+  return (
+    <td className={ "Event " }>
+      <img src={props.imgLink}></img>
+      <h5>{props.name}</h5>
+      <h6>{props.origin}</h6>
+      <button onClick={props.website}>Recipe Button</button>
+    </td>)
+}
+
+export default Event;
---
+++ src/components/Recipe.jsx
@@ -0,0 +1,24 @@
+import Event from './Event'
+
+const Recipe = () => {
+  return (
+    <div className="recipeGrid">
+      <table>
+
+        <td className="random">hello</td>
+        <Event />
+        <td className="random">hello</td>
+        <td className="random">hello</td>
+        <td className="random">hello</td>
+
+
+      </table>
+
+
+    </div>
+
+  )
+
+}
+
+export default Recipe;
