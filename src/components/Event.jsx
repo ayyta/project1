7f69a2af6af6ef2eca015815b7d9a4dbeb48db8e
Alvis Yong <yong.mark.alvis@gmail.com>
2023-09-20 17:14:48 -0700

    Finished creating gird element TODO: style the page more and figure out js for Recipe function

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -6,7 +6,7 @@ const Event = (props) => {
       <img src={props.imgLink}></img>
       <h5>{props.name}</h5>
       <h6>{props.origin}</h6>
-      <button onClick={props.website}>Recipe Button</button>
+      <button onClick={props.website}>View Recipe</button>
     </div>)
 }
 
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,19 +1,54 @@
 import Event from './Event'
 
+let recipeData = [
+  {
+    name:'Chicken Tikka Masala', imgLink: 'https://www.seriouseats.com/thmb/DbQHUK2yNCALBnZE-H1M2AKLkok=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/chicken-tikka-masala-for-the-grill-recipe-hero-2_1-cb493f49e30140efbffec162d5f2d1d7.JPG', origin: 'Indian', website: 'https://cafedelites.com/chicken-tikka-masala/'
+  }, {
+    name: 'Cheesecake', imgLink: 'https://sugarspunrun.com/wp-content/uploads/2019/01/Best-Cheesecake-Recipe-2-1-of-1-4.jpg', 
+    origin: 'Greece', website: 'https://sugarspunrun.com/best-cheesecake-recipe/',
+  }, {
+    name: '', imgLink: '', 
+    origin: '', website: '',
+  }, {
+    name: '', imgLink: '', 
+    origin: '', website: '',
+  }, {
+    name: '', imgLink: '', 
+    origin: '', website: '',
+  }, {
+    name: '', imgLink: '', 
+    origin: '', website: '',
+  }, {
+    name: '', imgLink: '', 
+    origin: '', website: '',
+  }, {
+    name: '', imgLink: '', 
+    origin: '', website: '',
+  }, {
+    name: '', imgLink: '', 
+    origin: '', website: '',
+  }, {
+    name: '', imgLink: '', 
+    origin: '', website: '',
+  }
+]
+
+
 const Recipe = () => {
+  let addToGrid = '';
+  recipeData.forEach( (food) => {
+    const { name, imgLink, origin, website } = food
+    addToGrid += `
+    <Event name=${name} imgLink=${imgLink} 
+    origin=${origin} website=${website}
+    
+    />`
+  })
+  console.log(addToGrid)
+
   return (
     <div className="recipeGrid">
-      <Event imgLink='photos/HannahandMasons.png' name='Chicken Masala' origin='Indian'/>
-      <Event />
-      <Event />
-      <Event />
-      <Event />
-      <Event />
-      <Event />
-      <Event />
-      <Event />
-      <Event />
-
+      {addToGrid}
     </div>
 
   )
